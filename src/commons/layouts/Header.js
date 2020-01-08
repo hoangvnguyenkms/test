d9fa01b0356f603baf7aba9f4d36fd67b66a28cb
hoangnguyenosd <hoang.nguyen.osd@gmail.com>
2020-01-08 17:05:47 +0700

    Create navbar

--- src/commons/layouts/Header.js
+++ src/commons/layouts/Header.js
@@ -8,27 +8,48 @@ export const Header = props => {
     const navbarList = [
         {
             id: 1,
-            name: 'One'
+            name: 'Home'
         },
         {
             id: 2,
-            name: 'Two'
+            name: 'Order'
         },
+        {
+            id: 3,
+            name: 'Warehouse'
+        },
+        {
+            id: 4,
+            name: 'Customer'
+        },
+        {
+            id: 5,
+            name: 'System'
+        },
+        {
+            id: 6,
+            name: 'Notification'
+        },
+        
     ];
     return (        
-        <Grid xs={12}>
-            <header class='header dark-background'>
-                <span>
-                    logo
-                </span>
-                <div class='navbar-user'>
-                    <Navbar 
-                        navbarList={navbarList}
-                    />
+        <Grid container spacing={0}>
+            <header className='header dark-background'>
+                <Grid item xs={12}>
                     <span>
-                        <LocationOnIcon color="secondary" />
+                        logo
                     </span>
-                </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <div className='navbar-user'>
+                        <Navbar 
+                            navbarList={navbarList}
+                        />
+                        <span>
+                            <LocationOnIcon color="secondary" />
+                        </span>
+                    </div>
+                </Grid>
             </header>
         </Grid>        
     )
